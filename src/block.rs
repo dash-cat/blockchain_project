c213bc5cac2f15190883553d262f46dca3d22118
Gregory <gregory@cat-science.org>
2024-04-25 13:07:03 +0000

    without PoW

--- src/block.rs
+++ src/block.rs
@@ -25,11 +25,22 @@ impl Block {
         block
     }
 
-    fn mine(&mut self, difficulty: usize) {
-        let prefix: String = std::iter::repeat("0").take(difficulty).collect();
-        while !self.hash.starts_with(&prefix) {
-            self.nonce += 1;
-            self.hash = self.calculate_hash();
-        }
+    fn calculate_hash(&self) -> String {
+        let mut hasher = Sha256::new();
+        hasher.update(self.index.to_string());
+        hasher.update(self.timestamp.to_string());
+        hasher.update(self.transactions.join(""));
+        hasher.update(&self.previous_hash);
+
+        let hash = hasher.finalize();
+        format!("{:x}", hash)
     }
+    
+    // fn mine(&mut self, difficulty: usize) {
+    //     let prefix: String = std::iter::repeat("0").take(difficulty).collect();
+    //     while !self.hash.starts_with(&prefix) {
+    //         self.nonce += 1;
+    //         self.hash = self.calculate_hash();
+    //     }
+    // }
 }
